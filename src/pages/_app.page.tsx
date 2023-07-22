4f03576f1dfb8c758f4ba0991755b4a30d0bf0c6
Steven Chen <Swunci@gmail.com>
2023-07-21 21:15:29 -0400

    feat: added google analytics and adsense

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -3,6 +3,8 @@ import '../styles/global.css';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { PayPalScriptProvider } from '@paypal/react-paypal-js';
 import type { AppProps } from 'next/app';
+import Script from 'next/script';
+import { GoogleAnalytics } from 'nextjs-google-analytics';
 
 const theme = createTheme({
   typography: {
@@ -15,6 +17,15 @@ const MyApp = ({ Component, pageProps }: AppProps) => (
     <PayPalScriptProvider
       options={{ clientId: process.env.NEXT_PUBLIC_CLIENT_ID }}
     >
+      <Script
+        id="Adsense-id"
+        data-ad-client="ca-pub-8556410874703049"
+        async
+        strategy="afterInteractive"
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8556410874703049"
+        crossOrigin="anonymous"
+      />
+      <GoogleAnalytics strategy="lazyOnload" />
       <Component {...pageProps} />
     </PayPalScriptProvider>
   </ThemeProvider>
